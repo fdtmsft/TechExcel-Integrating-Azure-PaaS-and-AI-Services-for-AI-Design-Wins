d82bd43fb7b762b20516ac484cf3d2b9f40ab5d3
Florian <fdudouet@microsoft.com>
2025-03-18 13:42:04 +0000

    added database methodes to api

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-klrx64m5krne4 FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-klrx64m5krne4;
+ALTER ROLE db_datawriter ADD MEMBER id-klrx64m5krne4;
 
